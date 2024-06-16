77e3e0bd4ef26d06122dbf8ed47222c4e3c48eec
jojessf <jojessfournier@gmail.com>
2024-06-15 20:36:10 -0400

    nyxabbs_v0.2.pl - ServerParms tidy, authfaildie, registrationopen, etc

--- nyxabbs_v0.2.pl
+++ nyxabbs_v0.2.pl
@@ -6,24 +6,27 @@ use strict;
 use IO::Socket::INET;                # https://metacpan.org/pod/IO::Socket::INET
 $SIG{CHLD} = sub { wait; };
 require("./NetBBSNyxa.pm"); 
-# use Net::BBS::Nyxa;
 # --------------------------------------------------------------------------- #
 my %ServerParms = (
-    # LocalHost => '127.0.0.1', # restrict to ip/iface
-    LocalPort => '6400',
-    Proto => 'tcp',
-    Listen => SOMAXCONN, # 4096
-    ReuseAddr => 1,
-    verbose => 1,
-    debugdir  => 'debug',
-    logdir    => 'log',
+    # LocalHost         => '127.0.0.1', # restrict to ip/iface
+    LocalPort         => '6400',
+    Proto             => 'tcp',
+    Listen            => SOMAXCONN, # 4096
+    ReuseAddr         => 1,
+    verbose           => 0,
+    # --------------------- #
+    authfaildie       => 0,
+    registrationopen  => 0,
+    # --------------------- #
+    debugdir          => 'debug',
+    logdir            => 'log',
+    confdir           => 'conf',
     PETSCIISplash00FI => "nyxabbs.splash",
     bbsmenumsg => "\r\n\r\n\@PCX{CYAN}~\@PCX{LIGHTBLUE}UwU\@PCX{CYAN}~\@PCX{PURPLE}".
                   "NyxaBBS\@PCX{LIGHTGRAY}:\@PCX{LIGHTGREEN}MainMenu".
                   "\@PCX{CYAN}~\@PCX{LIGHTGRAY}\@PCX{LIGHTBLUE}UwU\@PCX{CYAN}~\@PCX{LIGHTGRAY} \r\n\r\n" . 
                   "[\@PCX{RED}q\@PCX{LIGHTGRAY}]uit [\@PCX{CYAN}l\@PCX{LIGHTGRAY}]ogin [\@PCX{GREEN}r\@PCX{LIGHTGRAY}]egister [\@PCX{PURPLE}s\@PCX{LIGHTGRAY}]tats\r\n",  # $ServerParms{bbsmenumsg}
 );
-
 # --------------------------------------------------------------------------- #
 if ( -e $ServerParms{PETSCIISplash00FI} ) {
    open IF, "<" . $ServerParms{PETSCIISplash00FI};
@@ -33,7 +36,6 @@ if ( -e $ServerParms{PETSCIISplash00FI} ) {
    $ServerParms{PETSCIISplash00} = "\x9F".$ServerParms{PETSCIISplash00}."\x9B"
 }
 print "ingested " . $ServerParms{PETSCIISplash00FI} . "\t" . length($ServerParms{PETSCIISplash00}) . " B\n";
-# exit;
 # --------------------------------------------------------------------------- #
 # Server Socket 
 # --------------------------------------------------------------------------- #
@@ -42,19 +44,29 @@ my $server_socket = new IO::Socket::INET (
     %ServerParms
 );
 $server_socket || die $IO::Socket::errst; # works
+# --------------------------------------------------------------------------- #
+# NyxaBBS Init
+# --------------------------------------------------------------------------- #
+print "NyxaBBS Init :3\r\n";
+my $NyxaBBS = Net::BBS::Nyxa->new($server_socket, \%ServerParms);
+# --------------------------------------------------------------------------- #
+# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ #
+# --------------------------------------------------------------------------- #
 
 
-# --------------------------------------------------------------------------- #
-# IO::Socket::INET "sendbbs" wrapper sub~ 
-# --------------------------------------------------------------------------- #
+# my $userprofile = $NyxaBBS->getconf("user/test");
+# if ( ! $userprofile ) {
+#    $NyxaBBS->saveconf("user/test", {
+#       user => 'test',
+#       pass => 'meowmix',
+#    });
+# }
+
 
 # --------------------------------------------------------------------------- #
 # /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ #
 # --------------------------------------------------------------------------- #
-print "NyxaBBS Init :3\r\n";
-my $NyxaBBS = Net::BBS::Nyxa->new($server_socket, \%ServerParms);
-
-   $NyxaBBS->listen();
+$NyxaBBS->listen();
 # --------------------------------------------------------------------------- #
 # /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/ #
 # --------------------------------------------------------------------------- #
